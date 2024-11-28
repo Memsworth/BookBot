cb2b8c3e8e81d1c476e5682a9165422e6f8c3377
Abubakar Ahmed <abubakarmahfooz4479@outlook.com>
2024-11-29 01:06:15 +0530

    DRY & Code Smell way of writing a text parser

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text.RegularExpressions;
+
+var fileName = Path.Combine(Environment.CurrentDirectory.ToString(), "book.txt");
+
+if (!File.Exists(fileName))
+{
+    throw new FileNotFoundException(fileName);
+}
+
+var fileText= await File.ReadAllLinesAsync(fileName);
+
+var trackerDictionary = new Dictionary<char, int>();
+var wordTracker = new Dictionary<string, int>();
+
+
+foreach (var line in fileText)
+{
+    if(!string.IsNullOrEmpty(line))
+    {
+        var wordsInLine = line.Split(' ');
+
+        foreach (var word in wordsInLine)
+        {
+            var wordWithNoSpecialChar = Regex.Replace(word, @"(\s+|@|&|\*|\(|\)|<|>|#|\""|,|\.|;|:)", string.Empty).ToLower();
+            if(wordTracker.ContainsKey(wordWithNoSpecialChar))
+            {
+                wordTracker[wordWithNoSpecialChar]++;
+            }
+            else
+            {
+                wordTracker.Add(wordWithNoSpecialChar, 1);
+            }
+        }
+
+        foreach (var item in line)
+        {
+            if (trackerDictionary.ContainsKey(item))
+            {
+                trackerDictionary[item]++;
+            }
+            else
+            {
+                trackerDictionary.Add(item, 1);
+            }
+        }
+    }
+}
+
+foreach (var item in wordTracker.OrderByDescending(key => key.Value))
+{
+    Console.WriteLine($"{item.Key}: {item.Value}");
+}
